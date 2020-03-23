e21503405e1b7f04837cc4595612112a796d8030
lordiii <lordhallo123@web.de>
2020-03-23 17:05:05 +0100

    Optimizing + Remove clutter
    - Add Check for existing compressed files
    - Removed all the echo shit and trimmed it down

--- fastdl.sh
+++ fastdl.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
 
+COMPRESSEDCOUNTER=0
+PROCESSEDFILES=0
+
 rm -R /tmp/fastdltmp
 rm garrysmod/lua/autorun/server/resources.lua
 
 # Process all files in addons folder
 for i in $(find garrysmod/addons); do
+    PROCESSEDFILES=$((PROCESSEDFILES+1));
+    
     FILEPATH="$i"
     RAWPATH=${FILEPATH#garrysmod/addons/*/}
     # Check if path is not a directory and is not part of lua, gamemodes or already compressed
     if [[ ! -d "$i" && "$i" != *"lua/"* && "$i" != *"gamemodes/"* && "$i" != *".bz2"* ]]; then
-        echo "CURRENT FILE: $i"
         # Add to resources.lua
-        if [ "$i" != *"maps/"* ]; then
-            echo "CREATING 'resource.AddFile'"
+        if [ "$i" != "maps/"* ]; then
             echo "resource.AddFile(\"$RAWPATH\")" >> garrysmod/lua/autorun/server/resources.lua
         fi
         
         # Compress file to .bz2
-        echo "COMPRESSING"
-        /bin/bzip2 -k -f -z $FILEPATH
-        
-        echo ""
+        if [ ! -f "$i.bz2" ]; then
+            /bin/bzip2 -k -f -z $FILEPATH
+            COMPRESSEDCOUNTER=$((COMPRESSEDCOUNTER+1));
+        fi
     fi
 done
+
+echo "PROCESSED $PROCESSEDFILES FILES AND COMPRESSED $COMPRESSEDCOUNTER FILES"
+echo "COPYING FILES"
 
 # Make fastdltmp.tar.gz archive for faster upload to fastdl server
 for i in $(find garrysmod/addons); do
     FILEPATH="$i"
     RAWPATH=${FILEPATH#garrysmod/addons/*/}
     if [[ ! -d "$i" && "$i" == *".bz2"* ]]; then
-	    echo "COPYING $i"
         mkdir -p /tmp/fastdltmp/$(dirname $RAWPATH)
         cp "$i" /tmp/fastdltmp/$(dirname $RAWPATH)
     fi
